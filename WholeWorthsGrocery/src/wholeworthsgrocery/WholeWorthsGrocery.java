3a60016c6a8c903892b739a4d4f3d98c86ff41be
Stuart <Stuart@10.0.0.200>
2022-09-17 09:36:27 +0800

    New project

---
+++ WholeWorthsGrocery/src/wholeworthsgrocery/WholeWorthsGrocery.java
@@ -0,0 +1,17 @@
+
+package wholeworthsgrocery;
+
+/**
+ *
+ * @author Stuart
+ */
+public class WholeWorthsGrocery {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
