92babb266f4d097c3f5d7a96b99d636ced622701
Stuart <Stuart@10.0.0.200>
2022-09-19 21:19:23 +0800

    Error trapping added

--- WholeWorthsGrocery/src/wholeworthsgrocery/FXMLController.java
+++ WholeWorthsGrocery/src/wholeworthsgrocery/FXMLController.java
@@ -2,41 +2,23 @@ package wholeworthsgrocery;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class FXMLController {
 
     @FXML
-    private Button buttonCalculate;
-
-    @FXML
-    private Label labelBread;
-
-    @FXML
     private Label labelBreadItemTotal;
-
-    @FXML
-    private Label labelEggs;
 
     @FXML
     private Label labelEggsItemTotal;
 
     @FXML
-    private Label labelItemTotal;
-
-    @FXML
-    private Label labelMilk;
-
-    @FXML
     private Label labelMilkItemTotal;
-
-    @FXML
-    private Label labelQuantity;
-
-    @FXML
-    private Label labelTotal;
 
     @FXML
     private Label labelTotalCalculated;
@@ -52,14 +34,35 @@ public class FXMLController {
 
     @FXML
     void buttonCalculateOnAction(ActionEvent event) {
-        int eggsQty = Integer.parseInt(textFieldEggsQty.getText());
-        int milkQty = Integer.parseInt(textFieldMilkQty.getText());
-        int breadQty = Integer.parseInt(textFieldBreadQty.getText());
+        try {
+            int eggsQty = Integer.parseInt(textFieldEggsQty.getText());
+            int milkQty = Integer.parseInt(textFieldMilkQty.getText());
+            int breadQty = Integer.parseInt(textFieldBreadQty.getText());
         
         double eggsItemTotal = (eggsQty / 12 * 5.0) + (eggsQty % 12 * 0.5);
-        double milkTotal = (milkQty / 3 * 7.0) + (milkQty % )
+        double milkItemTotal = (milkQty / 3 * 7.0) + (milkQty % 3 * 2.5);
+        double breadItemTotal = (breadQty / 2 * 6.0) + (breadQty % 2 * 3.5);
         
-        labelEggsItemTotal.setText(String.valueOf(eggsItemTotal));
+        labelEggsItemTotal.setText("$" + String.valueOf(eggsItemTotal));
+        labelMilkItemTotal.setText("$" + String.valueOf(milkItemTotal));
+        labelBreadItemTotal.setText("$" + String.valueOf(breadItemTotal));
+        
+        double totalCalculated = eggsItemTotal + milkItemTotal + breadItemTotal;
+        
+        labelTotalCalculated.setText("$" + String.valueOf(totalCalculated));
+        
+        }
+        catch (NumberFormatException e) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Input Error");
+            alert.setHeaderText("Input Error");
+            alert.setContentText("Quantity must be an integer");
+            alert.showAndWait().ifPresent(rs -> {
+                if (rs == ButtonType.OK) {
+                    System.out.println("Pressed OK.");
+                }
+            });
+        }
     }
 
 }
