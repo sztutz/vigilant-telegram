96f5e7db1bcd83a9ee1ec68d20561a95fe902741
Stuart <Stuart@10.0.0.200>
2022-09-19 11:51:18 +0800

    FXML Controller implemented and working

--- WholeWorthsGrocery/src/wholeworthsgrocery/FXMLController.java
+++ WholeWorthsGrocery/src/wholeworthsgrocery/FXMLController.java
@@ -1,24 +1,58 @@
 package wholeworthsgrocery;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class FXMLController implements Initializable {
-    
+public class FXMLController {
+
+    @FXML
+    private Button buttonCalculate;
+
+    @FXML
+    private Label labelBread;
+
+    @FXML
+    private Label labelBreadItemTotal;
+
+    @FXML
+    private Label labelEggs;
+
+    @FXML
+    private Label labelEggsItemTotal;
+
+    @FXML
+    private Label labelItemTotal;
+
+    @FXML
+    private Label labelMilk;
+
+    @FXML
+    private Label labelMilkItemTotal;
+
+    @FXML
+    private Label labelQuantity;
+
+    @FXML
+    private Label labelTotal;
+
     @FXML
     private Label labelTotalCalculated;
-    
+
     @FXML
-    private void handleButtonCalculatePress(ActionEvent event) {
-        labelTotalCalculated.setText("Hello Mother Fucker!");
+    private TextField textFieldBreadQty;
+
+    @FXML
+    private TextField textFieldEggsQty;
+
+    @FXML
+    private TextField textFieldMilkQty;
+
+    @FXML
+    void handleButtonAction(ActionEvent event) {
+        labelTotalCalculated.setText("Hello Mother Fucker");
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        //TODO
-    }
+
 }
